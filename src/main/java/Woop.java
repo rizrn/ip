c3b04b8b8209b623c2bee64a46d7ee76c2b1a6c6
Jason Lim <89007417+rizrn@users.noreply.github.com>
2025-08-22 14:19:14 +0800

    Add greeting

---
+++ src/main/java/Woop.java
@@ -0,0 +1,11 @@
+public class Woop {
+    public static final String name = "Woop";
+    public static void main(String[] args) {
+        String startMessage = "Hello! I'm "
+                + name + "\n" +
+                "What can I do for you?";
+        String endMessage = "Bye. Hope to see you again soon!";
+        System.out.println(startMessage);
+        System.out.println(endMessage);
+    }
+}
