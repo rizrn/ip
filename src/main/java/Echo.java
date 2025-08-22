bfc746f6d8e3b5a71bd5c89865c6466a8b6d9ae7
Jason Lim <89007417+rizrn@users.noreply.github.com>
2025-08-22 14:55:40 +0800

    added EchoList class

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
 public class Echo {
     private Scanner sc;
+    private String userInput;
 
     public Echo() {
         this.sc = new Scanner(System.in);
+        this.userInput = "";
     }
 
     public void echoMessage() {
-        String userInput = this.sc.nextLine();
-        while (!checkExit(userInput)) {
-            System.out.println(userInput);
-            userInput = this.sc.nextLine();
+        this.userInput = this.sc.nextLine();
+        while (!checkExit()) {
+            System.out.println(this);
+            this.userInput = this.sc.nextLine();
         }
     }
 
-    private boolean checkExit(String text) {
+    private boolean checkExit() {
         String exit = "bye";
-        return text.equals(exit);
+        return this.userInput.equals(exit);
     }
 
-
+    @Override
+    public String toString() {
+        return this.userInput;
+    }
 }
---
+++ src/main/java/EchoList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class EchoList extends Echo {
+    private ArrayList<String> list;
+    public EchoList() {
+        super();
+        this.list = new ArrayList<>();
+    }
+
+
+}
